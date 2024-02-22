55fb745dd569ffa0decefe0b8364664febdee9f3
Juan Manuel Ospina Londoño <juanmasgr@gmail.com>
2024-02-21 21:02:24 -0500

    El primer paso es declarar las variables que voy a usar

---
+++ Ejercicio_operaciones_aritmeticas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_operaciones_aritmeticas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
